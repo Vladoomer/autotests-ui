5bfff5f9af0313de034cb12160bc85d3dd6718ca
Vlad Minashkin <minashkin@git.com>
2026-07-08 21:44:39 +0300

    Знакомство с PageFactory

--- components/courses/course_view_menu_component.py
+++ components/courses/course_view_menu_component.py
@@ -1,13 +1,16 @@
 from components.base_component import BaseComponent
 from playwright.sync_api import Page, expect
 
+from elements.button import Button
+
+
 class CourseViewMenuComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.menu_button = page.get_by_test_id('course-view-menu-button')
-        self.edit_menu_item = page.get_by_test_id('course-view-edit-menu-item')
-        self.delete_menu_item = page.get_by_test_id('course-view-delete-menu-item')
+        self.menu_button = Button(page, 'course-view-menu-button', "Menu button")
+        self.edit_menu_item = Button(page, 'course-view-edit-menu-item', "Edit button")
+        self.delete_menu_item = Button(page, 'course-view-delete-menu-item', "Delete button")
 
     def click_edit(self, index: int):
         expect(self.menu_button).to_be_visible()
