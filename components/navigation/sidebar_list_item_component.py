f78e969bdc4bd625b621637562be8b4983124bf9
Vlad Minashkin <minashkin@git.com>
2026-07-08 22:28:36 +0300

    Практика работы с Page Factory

--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -1,22 +1,25 @@
 from playwright.sync_api import Page, expect
 from typing import Pattern
 from components.base_component import BaseComponent
+from elements.button import Button
+from elements.icon import Icon
+from elements.text import Text
 
 
 class SidebarListItemComponent(BaseComponent):
     def __init__(self, page: Page, identifier: str):
         super().__init__(page)
 
-        self.icon = page.get_by_test_id(f"{identifier}-drawer-list-item-icon")
-        self.title = page.get_by_test_id(f"{identifier}-drawer-list-item-title-text")
-        self.button = page.get_by_test_id(f"{identifier}-drawer-list-item-button")
+        self.icon = Icon(page,f"{identifier}-drawer-list-item-icon", "Icon")
+        self.title = Text(page,f"{identifier}-drawer-list-item-title-text", "Title")
+        self.button = Button(page, f"{identifier}-drawer-list-item-button", "Button")
 
     def check_visible(self, text:str):
-        expect(self.icon).to_be_visible()
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text(text)
-        expect(self.button).to_be_visible()
-
+        self.icon.check_visible()
+        self.title.check_visible()
+        self.title.check_visible(text=text)
+        self.button.check_visible()
     def navigate(self, expected_url: Pattern[str]):
         self.button.click()
         self.check_current_url(expected_url)
+
--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,4 +1,5 @@
 from components.authentication.registration_form_component import RegistrationFormComponent
+from elements.button import Button
 from pages.base_page import BasePage
 from playwright.sync_api import Page, expect
 
@@ -6,7 +7,7 @@ class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.registration_button = page.get_by_test_id("registration-page-registration-button")
+        self.registration_button = Button(page, "registration-page-registration-button", "Registration")
         self.registration_form = RegistrationFormComponent(page)
 
     def click_registration_button(self):
